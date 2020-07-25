8b3697ac8aa20658cf16339e743c98e73e6594bb
Chris Pyles <cpyles@berkeley.edu>
2020-07-25 12:09:07 -0700

    added timeout

--- nbpdfexport/__init__.py
+++ nbpdfexport/__init__.py
@@ -13,14 +13,14 @@ import tempfile
 from pyppeteer import launch
 
 
-async def html_to_pdf(html_file, pdf_file):
+async def html_to_pdf(html_file, pdf_file, timeout=30000):
     """
     Convert arbitrary HTML file to PDF
     """
     browser = await launch(args=['--no-sandbox'])
     page = await browser.newPage()
     # Waiting for networkidle0 seems to let mathjax render
-    await page.goto(f'file:///{html_file}', {'waitUntil': ['networkidle0']})
+    await page.goto(f'file:///{html_file}', {'waitUntil': ['networkidle0'], 'timeout': timeout})
     # Give it *some* margins to make it look a little prettier
     # I just made these up
     page_margins = {
@@ -33,7 +33,7 @@ async def html_to_pdf(html_file, pdf_file):
     await browser.close()
 
 
-async def notebook_to_pdf(notebook_model, pdf_path):
+async def notebook_to_pdf(notebook_model, pdf_path, timeout=30000):
     """
     Convert given notebook model to PDF
     """
@@ -43,7 +43,7 @@ async def notebook_to_pdf(notebook_model, pdf_path):
     with tempfile.NamedTemporaryFile(suffix='.html') as f:
         f.write(exported_html.encode())
         f.flush()
-        await html_to_pdf(f.name, pdf_path)
+        await html_to_pdf(f.name, pdf_path, timeout=timeout)
 
 def main():
     argparser = argparse.ArgumentParser()
@@ -55,12 +55,19 @@ def main():
         'pdf_path',
         help='Path to output PDF file to'
     )
+    argparser.add_argument(
+        '--timeout',
+        nargs="?",
+        default=30000,
+        type=int,
+        help="Default timeout for loading pages in pyppeteer"
+    )
 
     args = argparser.parse_args()
 
     with open(args.notebook_path) as f:
         notebook_model = nbformat.read(f, as_version=4)
-    asyncio.get_event_loop().run_until_complete(notebook_to_pdf(notebook_model, args.pdf_path))
+    asyncio.get_event_loop().run_until_complete(notebook_to_pdf(notebook_model, args.pdf_path, timeout=args.timeout))
 
 if __name__ == '__main__':
     main()
